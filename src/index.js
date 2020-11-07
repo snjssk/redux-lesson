3c7ede8753cc0499628b87a66a909fbbef9fd251
snjssk <snjssk0403@gmail.com>
2020-11-07 23:40:47 +0900

    update

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ const store = configureStore({
 });
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
